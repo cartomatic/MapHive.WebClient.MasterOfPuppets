b1c2b52f43e64f093b5f05e19f7e1bf5f1d6d9d8
Dominik Mikiewicz <dominik@cartomatic.pl>
2016-07-10 06:15:14 +0200

    Port the main side nav viewport to mh and make it generic

--- MapHive.WebClient.MasterOfPuppets/classic/src/view/main/Main.js
+++ MapHive.WebClient.MasterOfPuppets/classic/src/view/main/Main.js
@@ -8,64 +8,18 @@
      * App's viewport
      */
     Ext.define('MasterOfPuppets.view.main.Main', {
-        extend: 'Ext.panel.Panel',
+        extend: 'mh.module.mainView.SideNavViewport',
 
     requires: [
         'Ext.container.Container',
         'Ext.layout.container.Card',
         'Ext.layout.container.HBox',
         'Ext.list.Tree',
-        'Ext.panel.Panel',
         'Ext.plugin.Viewport',
-        'MasterOfPuppets.view.main.MainModel',
         'MasterOfPuppets.view.main.MainController'
     ],
 
-    plugins: 'viewport',
-
-        controller: 'viewport',
-
-        viewModel: {
-            type: 'viewport'
-        },
-
-        border: false,
-
-        layout: {
-            type: 'hbox',
-            align: 'stretch',
-
-            // Tell the layout to animate the x/width of the child items.
-            animate: true,
-            animatePolicy: {
-                x: true,
-                width: true
-            }
-        },
-
-        items: [
-            {
-                xtype: 'treelist',
-                reference: 'navTreeList',
-                ui: 'navigation',
-                margin: '0 5 0 0', //t r b l
-                width: 250,
-                expanderFirst: false,
-                expanderOnly: false,
-                listeners: {
-                    selectionchange: 'onTreeListSelectionChange'
-                },
-                bind: {
-                    store: '{treeMenu}'
-                }
-            },
-            {
-                xtype: 'container',
-                reference: 'cardHolder',
-                layout: 'card',
-                flex: 1
-            }
-        ]
+        controller: 'viewport'
     });
 
 }());
--- MapHive.WebClient.MasterOfPuppets/classic/src/view/main/MainController.js
+++ MapHive.WebClient.MasterOfPuppets/classic/src/view/main/MainController.js
@@ -8,23 +8,18 @@
      * This is a simplistic example of a hosted app CLASSIC view. it simply sets up some
      */
     Ext.define('MasterOfPuppets.view.main.MainController', {
-        extend: 'Ext.app.ViewController',
+        extend: 'mh.module.mainView.SideNavViewportController',
         alias: 'controller.viewport',
 
         mixins: [
-            'mh.communication.MsgBus',
-            'mh.mixin.ModalMode',
-            'mh.mixin.Localisation',
-            'mh.mixins.RouteUtils'
+            'mh.mixin.CallMeParent',
+            'mh.mixin.Localisation'
         ],
 
     requires: [
-        'Ext.app.Application',
-        'Ext.app.route.Route',
-        'Ext.data.TreeStore',
         'Ext.panel.Panel',
         'Ext.util.History',
-        'MasterOfPuppets.model.NavigationTree',
+        'MasterOfPuppets.view.main.MainLocalisation',
         'MasterOfPuppets.view.applications.Applications',
         'MasterOfPuppets.view.dashboard.Dashboard',
         'MasterOfPuppets.view.localisations.Localisations',
@@ -32,52 +27,14 @@
         'MasterOfPuppets.view.users.Users'
     ],
 
-        //Note: routes are set up automatically on init, depending on the context
-
-
-        /**
-         * Called when the view is created
-         */
-        init: function () {
-
-            //grab the menu data first and
-            var menudata = this.prepareMenuData();
-
-            this.prepareRoutes(menudata);
-
-            this.loadMenu(menudata);
-
-            //it is required to init the History object prior to using it
-            Ext.util.History.init();
-
-            this.listen({
-                controller: {
-                    '#': {
-                        unmatchedroute: this.onUnmatchedRoute
-                    }
-                }
-            });
+        init: function(){
+            this.callMeParent('init', arguments);
         },
 
         /**
-         * Extracts routes off the menu data and registers them with the controller;
-         * @param menudata
+         * Prepares the menu data for the navigation tree list
+         * @returns {*[]}
          */
-        prepareRoutes: function(menudata){
-            var m = 0, mlen = menudata.length,
-                routes,r, rlen,
-                outRoutes = {};
-
-            for (m; m < mlen; m++){
-                routes = menudata[m].routes;
-                r = 0; rlen = routes.length;
-                for(r; r < rlen; r++){
-                    outRoutes[routes[r]] = 'onMatchedRoute'
-                }
-            }
-            this.setRoutes(outRoutes);
-        },
-
         prepareMenuData: function(){
             //Note: this can be data driven and based on the serverside roles / permissions configuration
             return [
@@ -119,142 +76,6 @@
                     leaf: true
                 }
             ];
-        },
-
-        /**
-         * Loads the appropriate left hand side menu data
-         */
-        loadMenu: function(data){
-            var vm = this.getViewModel(),
-                treeStore = vm.get('treeMenu');
-
-            //just rebind the root to load the data
-            treeStore.setRootNode({
-                children: data
-            });
-        },
-
-
-        /**
-         * unmatched route handler
-         */
-        onUnmatchedRoute: function (route) {
-
-            //properly handle EDIT MODE!
-            if (this.getModalModeActive()) {
-
-                //<debug>
-                console.warn('[ROUTER@Main]', 'prevented route adjustment - edit mode active!');
-                //</debug>
-
-                window.location.hash = this.getModalModeRouteSnapshot();
-                return;
-            }
-
-            this.redirectToDefaultRoute();
-        },
-
-        routesCheckupCache: null,
-
-        /**
-         * checks if a current rpute matches routes configured for a menu node
-         * @param {string} route
-         * @param {MasterOfPuppets.model.NavigationTree} n
-         * @returns {boolean}
-         */
-        checkIfRouteMatches: function(route, n){
-            this.routesCheckupCache = this.routesCheckupCache || {};
-
-            var viewRef = n.get('viewReference'),
-                routes,r,rlen;
-
-            if(!this.routesCheckupCache.hasOwnProperty(viewRef)){
-                this.routesCheckupCache[viewRef] = this.prepareRouteValidators(n.get('routes'));
-            }
-            routes = this.routesCheckupCache[viewRef];
-            r = 0; rlen = routes.length;
-
-            for(r; r < rlen; r++){
-                if(routes[r].recognizes(route))
-                    return true;
-            }
-
-            return false;
-        },
-
-        /**
-         * Matched route handler
-         */
-        onMatchedRoute: function(){
-
-            //since got here, the route should basically be recognised and there should be a view for it
-
-            //Note: this app only handles single routes (no piped ones)
-            var route = this.getCurrentHash();
-
-            var navList = this.lookupReference('navTreeList'),
-                treeStore = navList.getStore(),
-                me = this,
-                node = treeStore.getRoot().findChildBy(
-                    function(n){
-                        return me.checkIfRouteMatches(route, n);
-                    }
-                ),
-                viewRef,
-                cardHolder, cardLayout, currentView, newView;
-
-            //if a node has been found it means there is a matching menu item and a view for a route
-            if(node){
-
-                viewRef = node.get('viewReference');
-
-                node.set('currentRoute', route);
-
-                //highlight an item on a list
-                navList.setSelection(node);
-
-                //turn on the view
-                cardHolder = this.lookupReference('cardHolder');
-                cardLayout = cardHolder.getLayout();
-                currentView = cardLayout.getActiveItem();
-                newView = this.lookupReference(viewRef);
-
-                //if the new view has not yet been created, do create it!
-                if(!newView){
-
-                    if(node.get('view')){
-                        newView = Ext.create(node.get('view'), {
-                            reference: viewRef
-                        });
-                    }
-                    else {
-                        newView = Ext.create('Ext.panel.Panel', {
-                            title: route,
-                            html: '<span style ="color:red;">No view defined for <span style="font-weight: bold;">' + route + '</span></span> yet!'
-                        });
-                    }
-
-                    cardHolder.add(newView);
-                }
-
-                if(currentView && newView !== currentView){
-                    cardLayout.setActiveItem(newView);
-                }
-            }
-            else {
-                //Note: this could pretty much happen during the dev. in production this should never be the case ;)
-                throw new Error('BOOOOM.... View ref for "' + route + '" route has not been found...');
-            }
-        },
-
-        /**
-         * Tree list selection changed callback handdler
-         * @param treelist
-         * @param selected
-         * @param eOpts
-         */
-        onTreeListSelectionChange: function(treelist, selected, eOpts){
-            this.redirectTo(selected.get('currentRoute') || selected.get('routes')[0], false); //do not force hash update if the same
         }
     });
 
